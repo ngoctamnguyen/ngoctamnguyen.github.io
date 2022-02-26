036da4e65ef5cb9e786e31ea0fc41e3e0fcf5759
ngoctamnguyen <ngocnguyen@miu.edu>
2022-02-25 21:31:15 -0600

    Update powTest.js

--- d14demo/powTest.js
+++ d14demo/powTest.js
@@ -11,3 +11,42 @@ describe("pow", function () {
     });
 
 });
+
+/* global assert maxOfThree sum multiply findLongestWord reverseArray reverseArrayInPlace scoreExams generateArray */
+
+/* 1.	1.	Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.  */
+describe("maxOfThree", function () {
+    it("tests 1 2 3", function () {
+        assert.strictEqual(maxOfThree(1, 2, 3), 3);
+    });
+    it("tests 1 3 2", function () {
+        assert.strictEqual(maxOfThree(10, 30, 20), 30);
+    });
+    it("tests 2 1 3", function () {
+        assert.strictEqual(maxOfThree(2, 1, 3), 3);
+    });
+    it("tests 2 3 1", function () {
+        assert.strictEqual(maxOfThree(22, 33, 11), 33);
+    });
+    it("tests 3 2 1", function () {
+        assert.strictEqual(maxOfThree(3, 2, 1), 3);
+    });
+    it("tests 3 1 2", function () {
+        assert.strictEqual(maxOfThree(333, 111, 222), 333);
+    });
+    it("tests -1 -2 -3", function () {
+        assert.strictEqual(maxOfThree(-1, -2, -3), -1);
+    });
+    it("tests -1 -2 -2", function () {
+        assert.strictEqual(maxOfThree(-1, -2, -2), -1);
+    });
+    it("tests 5 5 -1", function () {
+        assert.strictEqual(maxOfThree(5, 5, -1), 5);
+    });
+    it("tests -2 0 -2", function () {
+        assert.strictEqual(maxOfThree(-2, 0, -2), 0);
+    });
+    it("tests 6 6 6", function () {
+        assert.strictEqual(maxOfThree(6, 6, 6), 6);
+    });
+});
