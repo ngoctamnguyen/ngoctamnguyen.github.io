23a303bae41d42fc371c68891d29d349d643c18c
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-03 21:10:28 -0600

    Create filterRangeInPlace.js

---
+++ demos/filterRangeInPlace.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let arr = [5, 3, 8, 1];
+function filterRangeInPlace(arr,a,b) {
+    arr.forEach((item,index) => {(item < a || item > b) ? arr.splice(index,1) : ""})
+}
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);// [1,3]
