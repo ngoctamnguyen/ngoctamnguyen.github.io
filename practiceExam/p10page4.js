1d8376862fb19863fbf14b45498aea595c47f1f6
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-06 17:28:11 -0600

    Create p10page4.js

---
+++ practiceExam/p10page4.js
@@ -0,0 +1,8 @@
+
+
+let array= [{name:"ram",age:29}, {name:"syam",age:40}, {name:"ganga",age:19}];
+
+function getAverageAge(arr) {
+    return arr.reduce((prev,user) => prev + user.age,0);
+}
+console.log( getAverageAge(array));
