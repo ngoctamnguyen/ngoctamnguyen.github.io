4c4f31376ffc5be44dffd5395a896a19edc5e452
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-06 17:26:37 -0600

    Update filterRangeInPlace.js

--- demos/filterRangeInPlace.js
+++ demos/filterRangeInPlace.js
@@ -2,7 +2,8 @@
 
 let arr = [5, 3, 8, 1];
 function filterRangeInPlace(arr,a,b) {
-    arr.forEach((item,index) => {(item < a || item > b) ? arr.splice(index,1) : ""})
+    arr.forEach((item, index) => {if (item < a || item > b) arr.splice(index,1);});
+    //arr.forEach((item,index) => {(item < a || item > b) ? arr.splice(index,1) : ""})
 }
 filterRangeInPlace(arr, 1, 4);
 console.log(arr);// [1,3]
