9e7f34c6d63d840401e5ab7ef47cba6f1eac2961
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-06 17:26:55 -0600

    Create split_Join.js

---
+++ demos/split_Join.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function camelize(str) {
+    if (str == "") {
+        return "";
+    }
+    let arr = str.split(" ");
+    let newString = "";
+    arr.forEach((item,index) => {
+        if (index == 0 &&  str[0] != "-") {
+            console.log(index);
+            newString += item[0].toLowerCase() + item.slice(1);
+        }
+        else {
+            console.log(index);
+            newString += item[0].toUpperCase() + item.slice(1)
+        }
+    });
+    return newString;
+}
+console.log(camelize("nguyen ngoc tam")); //myNameIsTam
