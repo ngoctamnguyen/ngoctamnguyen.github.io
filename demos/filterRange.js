a9536df0e8e82e87e64d5767c7e877845b1a2bef
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-06 17:26:34 -0600

    Update filterRange.js

--- demos/filterRange.js
+++ demos/filterRange.js
@@ -3,7 +3,7 @@
 let arr = [5, 3, 8, 1];
 function filterRange(arr,a,b) {
     let newArr = [];
-    arr.forEach(item => {(item >= a && item <= b) ? newArr.push(item) : "";})
+    arr.forEach(item => {if (item >= a && item <= b) newArr.push(item);});
     return newArr;
 }
 let filtered = filterRange(arr, 1, 4);
