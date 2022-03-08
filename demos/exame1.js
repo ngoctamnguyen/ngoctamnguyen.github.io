e0fb0c2365f8855409a755c7fe4e8db25987a5ae
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-07 20:16:42 -0600

    Create exame1.js

---
+++ demos/exame1.js
@@ -0,0 +1,24 @@
+"use strict";
+let user1 = [{x:1}, {y:6}, {z:8}];
+let user2 = {x:1, y:6, z:8};
+function foo1 () {
+    const  arr = [];
+    for (const element of user1) {arr.push(element);}
+    return arr;
+}
+console.log(foo1());
+
+
+function foo2 () {
+    const  arr = [];
+    for (const element in user1) {arr.push(element);}
+    return arr;
+}
+console.log(foo2());
+
+function foo3 () {
+    const  arr = [];
+    for (const element in user2) {arr.push(element);}
+    return arr;
+}
+console.log(foo3());
