c2b1909714feccb872f4c0a4885e52ba384913e9
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-10 20:59:37 -0600

    Update passwords.js

--- d33callContext/passwords.js
+++ d33callContext/passwords.js
@@ -6,27 +6,33 @@
 function askPassword(ok, fail, password) {
   let pass = 'rockstar';
   if (pass === password) {
-      return ok;
+      return ok();
   } else {
-      return fail;
+      return fail();
   }
 }
 
 let user = {
     name: 'John',
 
-    loginOk() {
-      return  this.name + ' logged in' ;
+    loginOk: function loginOk() {
+      return  `${this.name} logged in`;
     },
 
-    loginFail() {
-      return this.name + ' failed to log in' ;
+    loginFail: function() {
+      return `${this.name} failed to log in` ;
     }
 
 };
 
-let test = askPassword(user.loginOk.call(user), user.loginFail.call(user), 'rokstar');
-console.log(test);
+console.log(askPassword(() => user.loginOk(), () => user.loginFail(), "rockstar"));
+console.log(askPassword(user.loginOk.bind(user), user.loginFail.bind(user), "rockstar"));
+console.log(askPassword(() => user.loginOk.call(user), () =>user.loginFail.call(user), "rockstar"));
+console.log(askPassword(() => user.loginOk.apply(user), () =>user.loginFail.apply(user, []), "rockstar"));
+
+// console.log(askPassword(user.loginOk.call(user), user.loginFail.call(user), 'rockstar')); //error: TypeError: ok is not a function
+// console.log(askPassword(user.loginOk.apply(user), user.loginFail.apply(user), 'rockstar')); //error: TypeError: ok is not a function
+console.log("**************************************************************************")
 
 function askPassword2(ok, fail, password) {
   let pass = 'rockstar';
@@ -48,8 +54,21 @@ let user2 = {
   }
 };
 
+console.log(askPassword2(() => user2.login(true), () => user2.login(false), "rockstar")); 
+
+// console.log(askPassword2(user2.login.call(user2), user2.login.call(user2), 'rockstar')); //error TypeError: ok is not a function
+// console.log(askPassword2(user2.login.apply(user2), user2.login.apply(user2), 'rockstar')); //error TypeError: ok is not a function
+
+/* WHY ERROR?
+Functions provide a built-in method bind that sets the value of ‘this’ for the function
+➢ Similar to wrapper in that have a function call with parameters that is returned but not executed
+➢ Similar to call and apply except that bind does not execute the function
+➢ Returns ‘a’ new function object with the new value of ‘this’ as the context
+*/
+
+
 //askPassword2(() => user2.login(true), () => user2.login(false), 'rocstar');
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-//module.exports = {askPassword, user, askPassword2, user2 }; //add all of your function names here that you need for the node mocha tests
+module.exports = {askPassword, user, askPassword2, user2 }; //add all of your function names here that you need for the node mocha tests
