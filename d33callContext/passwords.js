a70a4be8ea7128fcc103ddf5014000287062d7f1
ngoctamnguyen <ngocnguyen@miu.edu>
2022-03-11 21:11:15 -0600

    Update passwords.js

--- d33callContext/passwords.js
+++ d33callContext/passwords.js
@@ -71,4 +71,4 @@ Functions provide a built-in method bind that sets the value of ‘this’ for t
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-module.exports = {askPassword, user, askPassword2, user2 }; //add all of your function names here that you need for the node mocha tests
+//module.exports = {askPassword, user, askPassword2, user2 }; //add all of your function names here that you need for the node mocha tests
